0e609ab507598837a68d80579dc242dde089d483
MoeBrowne <oliver.browne@uplyme.com>
2015-04-14 22:24:11 +0100

    Added the main image management tool, currently it can only download a Raspbian image but it is aware if it's got the latest version of it

---
+++ manager.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+declare -A Images
+
+Images['Raspbian']="http://downloads.raspberrypi.org/raspbian_latest"
+
+#Regex
+regexETag="ETag: \"([a-z0-9\-]+)\""
+regexSize="Content-Length: ([0-9]+)"
+
+#Determine which image to download
+IMAGE_NAME="$1"
+IMAGE_URL="${Images[$IMAGE_NAME]}"
+
+#Get the HTTP headers for the image
+IMAGE_HEADERS=`curl -sIL "$IMAGE_URL"`
+
+#Get the ETag
+[[ $IMAGE_HEADERS =~ $regexETag ]]
+IMAGE_ETAG="${BASH_REMATCH[1]}"
+
+#Get the image size
+[[ $IMAGE_HEADERS =~ $regexSize ]]
+IMAGE_SIZE="${BASH_REMATCH[1]}"
+
+IMAGE_DIR="images/$IMAGE_NAME/$IMAGE_ETAG"
+IMAGE_FILE="$IMAGE_DIR/image.img.zip"
+
+#Check if we already have this version
+if [ ! -f $IMAGE_FILE ]; then
+	#Make the directory to store the image
+	mkdir -p "$IMAGE_DIR"
+	
+	#Download the image
+	curl -sL "$IMAGE_URL" | pv -s "$IMAGE_SIZE" -cN "Download" >  "$IMAGE_FILE"
+fi
+
