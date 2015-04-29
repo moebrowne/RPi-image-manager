b5a9d9e6618404d0d0c72800dbb985d6f5418f80
MoeBrowne <oliver.browne@uplyme.com>
2015-04-29 22:03:31 +0100

    Added a text colour library

---
+++ utils/colours.sh
@@ -0,0 +1,13 @@
+
+# Colours
+COLOUR_RST='\e[0m'
+COLOUR_RED='\e[0;31m'
+COLOUR_GRN='\e[0;32m' # Green
+COLOUR_YEL='\e[0;33m' # Yellow
+COLOUR_BLU='\e[0;34m' # Blue
+COLOUR_PUR='\e[0;35m' # Purple
+COLOUR_CYN='\e[0;36m' # Cyan
+COLOUR_GRY='\e[2m' # Gray
+
+# Text backgrounds
+COLOUR_RED_BAK='\e[0;101m' # Red
