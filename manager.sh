6c0b2c5fbae3a01eb027ef78c447c18d32fc7ffd
MoeBrowne <oliver.browne@uplyme.com>
2015-11-07 16:35:34 +0000

    Bumped version to 0.3.1

--- manager.sh
+++ manager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "-----------------------------------------"
-echo "  Raspberry Pi Image Manager (RIM) v0.3"
+echo " Raspberry Pi Image Manager (RIM) v0.3.1 "
 echo "-----------------------------------------"
 
 # Get the source directory
