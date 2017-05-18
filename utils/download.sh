e41a969c2d0c136caf10f0c920b3781f3498e042
MoeBrowne <oliver.browne@uplyme.com>
2017-05-18 21:20:16 +0100

    Localised variables and made the download function return numeric values

--- utils/download.sh
+++ utils/download.sh
@@ -1,14 +1,15 @@
 function download() {
 
-    local IMAGE_URL="$1"
+    local imageDownloadURL="$1"
+    local imageSavePath="$2"
 
     echo -en "$CLI_PREFIX Fetching meta data..."
 
     #Get the actual download URL of the image
-    IMAGE_URL=`curl -sIL "$IMAGE_URL" -o /dev/null -w %{url_effective}`
+    imageDownloadURL=`curl -sIL "$imageDownloadURL" -o /dev/null -w %{url_effective}`
 
     #Get the HTTP headers for the image
-    IMAGE_HEADERS=`curl -sI "$IMAGE_URL"`
+    IMAGE_HEADERS=`curl -sI "$imageDownloadURL"`
 
     #Get the HTTP response code
     [[ $IMAGE_HEADERS =~ $regexHTTPCode ]]
@@ -27,57 +28,18 @@ function download() {
     [[ $IMAGE_HEADERS =~ $regexSize ]]
     IMAGE_SIZE="${BASH_REMATCH[1]}"
 
-    #Get the image name
-    [[ $IMAGE_HEADERS =~ $regexFileName ]]
-    IMAGE_FILENAME="${BASH_REMATCH[1]}"
-
-    # Check we could determine the image file name
-    if [ "$IMAGE_FILENAME" = "" ]; then
-        # Default to a generic  name
-        IMAGE_FILENAME="image"
-    fi
-
-    #Set the image paths
-    IMAGE_CACHE_DIR="$selectedPath/cache"
-    IMAGE_FILE="$IMAGE_CACHE_DIR/$IMAGE_FILENAME"
-
     #Check if we already have this version
-    if [ ! -f "$IMAGE_FILE" ]; then
-        #Make the directory to store the image
-        mkdir -p "$IMAGE_CACHE_DIR"
-
-        echo -e "$CLI_PREFIX Downloading image..."
-
+    if [ ! -f "$imageSavePath" ]; then
         #Download the image
-        curl -sL "$IMAGE_URL" | pv -s "$IMAGE_SIZE" -cN "Download" >  "$IMAGE_FILE"
+        curl -sL "$imageDownloadURL" | pv -s "$IMAGE_SIZE" -cN "Download" > "$imageSavePath"
+        return 0
     else
-        echo -e "$CLI_PREFIX Using cache"
+        return 2
     fi
 
     # Check the file was created
-    if [ ! -f "$IMAGE_FILE" ]; then
-        echo -e "$CLI_PREFIX Something went wrong.. The image wasn't downloaded"
-        exit
+    if [ ! -f "$imageSavePath" ]; then
+        return 1
     fi
-
-    # Check if a SHA1 hash has been defined for this image
-    IMAGE_HASH="$(<"$selectedPath/hash")"
-
-    if [ "$IMAGE_HASH" != "" ]; then
-
-        # Hash the downloaded image
-        IMAGE_HASH_ACTUAL=$(pv -paeWcN "Checking Hash (SHA1)" "$IMAGE_FILE" | sha1sum |  grep -Eo "^([^ ]+)")
-
-        # Check the hashes match
-        if [ "$IMAGE_HASH" != "$IMAGE_HASH_ACTUAL" ]; then
-            echo -e "$CLI_PREFIX Hashes mismatch! [$IMAGE_HASH != $IMAGE_HASH_ACTUAL]"
-            exit 1
-        else
-            echo -e "$CLI_PREFIX Hash OK"
-        fi
-    fi
-
-    # Return the path to the image file
-    IMAGE_FILE_PATH="$IMAGE_FILE"
 
 }
